ac24042d8d2081450388134715a5351919a70ad9
ethan <workoutwrecker@gmail.com>
2025-03-05 17:52:50 +0800

    fix: keep back button fixed

--- src/app/refer/end/page.tsx
+++ src/app/refer/end/page.tsx
@@ -1,8 +1,8 @@
-// /src/app/refer/end/page.tsx
 'use client';
 
 import Head from 'next/head';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 export default function EndPage() {
   const router = useRouter();
@@ -15,23 +15,38 @@ export default function EndPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/x-icon" href="/refer/end/favicon.jpg" />
       </Head>
-      <div
-        className="w-screen h-screen flex justify-center items-center relative"
-        style={{
-          backgroundImage: "url('/refer/end.jpg')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundAttachment: "fixed",
-        }}
-      >
-        <button
-          onClick={() => router.push('/')}
-          className="absolute bottom-8 left-1/2 transform -translate-x-1/2 bg-gray-700 text-white py-2 px-4 rounded-md hover:bg-gray-600 transition-all duration-300"
-        >
-          Back to Main Page
-        </button>
+
+      {/* Background Image */}
+      <div className="relative w-screen h-screen flex justify-center items-center">
+        <div className="absolute inset-0">
+          {/* Desktop - Contain */}
+          <Image
+            src="/refer/end.jpg"
+            alt="Background Desktop"
+            layout="fill"
+            objectFit="contain"
+            className="hidden md:block"
+            priority
+          />
+          {/* Mobile - Cover */}
+          <Image
+            src="/refer/end.jpg"
+            alt="Background Mobile"
+            layout="fill"
+            objectFit="cover"
+            className="block md:hidden"
+            priority
+          />
+        </div>
       </div>
+
+      {/* Back to Main Page Button */}
+      <button
+        onClick={() => router.push('/')}
+        className="fixed bottom-8 left-1/2 transform -translate-x-1/2 bg-gray-700 text-white py-2 px-4 rounded-md hover:bg-gray-600 transition-all duration-300"
+      >
+        Back to Main Page
+      </button>
     </>
   );
 }
